15cc3f73e37b9d7588798f7b6a0c2143173e5589
Charan Kurupudi <147598632+CharanKurupudi@users.noreply.github.com>
2024-07-31 15:47:34 +0530

    import java.util.Scanner; public class Addition{ 	public static void main(String[] args){ 		int a,b,c; 		//An object of Scanner class
    		Scanner read = new Scanner(System.in);
    		a = read.nextInt();
    		b = read.nextInt();
    		c = a / b;
    		System.out.println(c);
    	}
    }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Addition{
+	public static void main(String[] args){
+		int a,b,c;
+		//An object of Scanner class
+
+		Scanner read = new Scanner(System.in);
+		a = read.nextInt();
+		b = read.nextInt();
+		c = a / b;
+		System.out.println(c);
+	}
+}
